14d7779aa4e4c061f2c991168d976a6992899643
Andrea Ricchi <andrea.ricchi@amarulasolutions.com>
2025-06-12 09:37:42 +0200

    Add BottomBar component
    This is used in the default page to display bottom bar info such as
    author, event and current page number.
    
    Signed-off-by: Andrea Ricchi <andrea.ricchi@amarulasolutions.com>

---
+++ components/BottomBar.vue
@@ -0,0 +1,28 @@
+<script>
+</script>
+
+<template>
+    <div class="bottom-bar flex-items-center">
+        <dev class="w-1/3 flex justify-start">
+            {{ $slidev.configs.author || '' }} <div class="divider"></div> Amarula Solutions
+        </dev>
+        <dev class="w-1/3 flex justify-center">
+            {{ $slidev.configs.event || '' }}
+        </dev>
+        <dev class="w-1/3 flex justify-end">
+            <div>
+                <SlideCurrentNo /> /
+                <SlidesTotal />
+            </div>
+        </dev>
+    </div>
+</template>
+
+<style scoped>
+.divider {
+    width: 1px;
+    height: 1em;
+    background-color: black;
+    margin: 3px 10px;
+}
+</style>
