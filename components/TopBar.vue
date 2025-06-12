a5716e96df4c84c4a9902ba43e3e7330476c2f41
Andrea Ricchi <andrea.ricchi@amarulasolutions.com>
2025-06-12 11:08:00 +0200

    Add TopBar component
    This is used in the default layout to display company logo and page
    title in a well designed way.
    
    Signed-off-by: Andrea Ricchi <andrea.ricchi@amarulasolutions.com>

---
+++ components/TopBar.vue
@@ -0,0 +1,26 @@
+<script>
+import { useDarkMode } from '@slidev/client'
+
+const { isDark } = useDarkMode()
+</script>
+
+<template>
+    <div class="top-bar flex flex-row space-x-8 items-end">
+        <img :src="isDark ? '../images/logo-small-white.svg' : '../images/logo-small-dark.svg'"
+            class="w-16 ml-4" alt="Amarula Logo" />
+
+        <div class="w-full flex-col">
+            <div class="title text-3xl -my-2">
+                <slot name="pageTitle" />
+            </div>
+            <div class="bg-accent divider rounded-sm"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.divider {
+    width: 98%;
+    height: 4px;
+}
+</style>
